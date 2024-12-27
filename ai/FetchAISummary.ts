11d4c3d9ab16a2ac83533f21e3839ae717c6d7be
chanakyagt <chanakyagattu2023@gmail.com>
2024-12-27 14:20:29 +0530

    removed openAI api

--- ai/FetchAISummary.ts
+++ ai/FetchAISummary.ts
@@ -5,7 +5,7 @@ import { createOpenAI } from '@ai-sdk/openai';
 const openai = createOpenAI({
   // custom settings, e.g.
   compatibility: 'strict',
-  apiKey:'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' // strict mode, enable when using the OpenAI API
+  apiKey:process.env.OPENAI_API_KEY // strict mode, enable when using the OpenAI API
 });
 export const AI=async (book_title,book_author,type)=>{
   let pmt=`write a brief overview of novel ${book_title} by ${book_author} don't give the entire plot just a reader friendly overview`
